37842a0e8696bcf63e9fe15371a788d4460caae1
Wesly Daud <weslydaud02@gmail.com>
2023-05-25 12:08:22 +0700

    create and update layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,51 @@
+import { NavLink, Outlet } from "react-router-dom";
+import { actions } from "../features/todos/todosSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+const Layout = () => {
+  const userInput = useSelector((state) => state.todos.userInput);
+  const dispatch = useDispatch();
+
+  const handleCreateTodo = (e) => {
+    e.preventDefault();
+    dispatch(actions.createTodo());
+  };
+
+  const handleSetUserInput = (userInput) => {
+    dispatch(actions.setUserInput({ userInput }));
+  };
+
+  return (
+    <div className="container mx-auto mt-10 px-4 max-w-fit">
+      <h1 className="text-center font-bold text-3xl mb-16">What's the plan for today?</h1>
+      <form className="flex gap-2 mb-12" onSubmit={handleCreateTodo}>
+        <input type="text" value={userInput} onChange={(e) => handleSetUserInput(e.target.value)} placeholder="What to do" className="p-2 w-full border-gray-300 border-solid border-2 rounded" />
+        <input type="submit" className="btn bg-[#6558F5] text-white font-medium w-20" value="Add" />
+      </form>
+      <nav className="mt-4">
+        <ul className="flex gap-2">
+          <li>
+            <NavLink to={"/"} className={({ isActive }) => (isActive ? "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-[#1AAE9F]" : "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-slate-500")}>
+              All
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/active"} className={({ isActive }) => (isActive ? "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-[#1AAE9F]" : "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-slate-500")}>
+              Active
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/completed"} className={({ isActive }) => (isActive ? "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-[#1AAE9F]" : "rounded-full text-white font-medium text-sm px-2.5 py-0.5 bg-slate-500")}>
+              Completed
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <section className="mt-4">
+        <Outlet />
+      </section>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { HiTrash, HiPencilAlt } from "react-icons/hi";
+import { useDispatch } from "react-redux";
+import { actions } from "../features/todos/todosSlice";
+
+const Todo = ({ todo }) => {
+  const dispatch = useDispatch();
+
+  const handleToggle = (id) => {
+    dispatch(actions.toggleTodo({ id }));
+  };
+
+  const handleRemove = (id) => {
+    dispatch(actions.deleteTodo({ id }));
+  };
+
+  const handleEdit = (id, content) => {
+    dispatch(actions.editTodo({ id, content }));
+  };
+
+  const [edit, setEdit] = useState(false);
+  const [editedContent, setEditedContent] = useState(todo.content);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSave();
+    }
+  };
+
+  const handleSave = () => {
+    handleEdit(todo.id, editedContent);
+    setEdit(false);
+  };
+
+  useEffect(() => {
+    setEditedContent(todo.content);
+  }, [todo.content]);
+
+  return (
+    <div className="flex justify-between p-2 bg-white rounded mb-4 items-center border border-gray-300">
+      <div className="flex gap-1 items-center">
+        <input type="checkbox" name="completed" checked={todo.completed} onChange={() => handleToggle(todo.id)} style={{ width: "1.2rem", height: "1.2rem" }} />
+        <label htmlFor="completed"></label>
+      </div>
+      <div className={`flex-grow ${todo.completed ? "line-through" : ""}`}>
+        {edit ? <input type="text" name="content" value={editedContent} onChange={(e) => setEditedContent(e.target.value)} onKeyDown={handleKeyDown} /> : <p>{todo.content}</p>}
+      </div>
+      <div className="flex gap-2 items-center">
+        <button className="btn btn-transparent" onClick={() => setEdit((value) => !value)}>
+          <HiPencilAlt style={{ color: "#4B5C6B", width: "1.2rem", height: "1.2rem" }} />
+        </button>
+        <button className="btn btn-transparent" onClick={() => handleRemove(todo.id)}>
+          <HiTrash style={{ color: "#4B5C6B", width: "1.2rem", height: "1.2rem" }} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
