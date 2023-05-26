f79015d90a3c82a5cfb9d9ac6097ddfd4f76391a
Wesly Daud <weslydaud02@gmail.com>
2023-05-26 10:46:22 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./index.css";
 import Layout from "./components/Layout";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 function App() {
+  const [formvisibility, setFromvisibility] = useState(false);
+
   return (
     <main className="container">
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout formvisibility={formvisibility} />} />
       </Routes>
     </main>
   );
